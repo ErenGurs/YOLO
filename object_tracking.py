37060de48682755fef80753908077f10473be909
ErenGurs <eren.gurses@gmail.com>
2023-07-03 17:44:38 -0700

    iterate for loop over both boxes and class_ids

--- object_tracking.py
+++ object_tracking.py
@@ -12,9 +12,9 @@ from object_detection import ObjectDetection
 od = ObjectDetection()  # Default: yolov3
 #od = ObjectDetection("models/yolov2-tiny.weights", "models/yolov2-tiny.cfg")
 
-#cap = cv2.VideoCapture("videos/los_angeles.mp4")
+cap = cv2.VideoCapture("videos/los_angeles.mp4")
 #cap = cv2.VideoCapture("videos/softball.mp4")
-cap = cv2.VideoCapture("videos/softball2.mp4")
+#cap = cv2.VideoCapture("videos/softball2.mp4")
 #cap = cv2.VideoCapture("videos/dog.mp4")
 
 while True:
@@ -28,17 +28,17 @@ while True:
 
     # Detect objects on frame
     (class_ids, scores, boxes) = od.detect(frame)
-    idx=0
     font = cv2.FONT_HERSHEY_PLAIN  # Font of the text put above rectangle
-    for box in boxes:
-        detected_class_label = od.classes[class_ids[idx]]
+    #for (box, class_id, score) in zip(boxes, class_ids, scores):
+    for (box, class_id) in zip(boxes, class_ids):
+        detected_class_label = od.classes[class_id]
         (x, y, w, h) = box
         cx = int((x + x + w) / 2)
         cy = int((y + y + h) / 2)
         center_points_cur_frame.append((cx, cy))
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        #cv2.putText(frame, detected_class_label + "(" + f'{score:.2f}' + ")", (x, y+20), font, 2, (255,255,255), 2)
         cv2.putText(frame, detected_class_label, (x, y+20), font, 2, (255,255,255), 2) 
-        idx+=1
 
     cv2.imshow("Frame", frame)
 
